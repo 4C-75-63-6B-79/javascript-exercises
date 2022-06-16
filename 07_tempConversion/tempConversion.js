eae345833ac8b1e217346cff127d21c3615f9f5b
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 23:57:57 +0530

    Seventh exercise completed with all the test case passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 5 / 9;
+  return temp.toFixed(1) - 0; // subtracting 0 to conver the returned value to number since the toFixed returns a string
+  // can also use Number(temp.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * 9 / 5 + 32;
+  return Number(temp.toFixed(1));
 };
 
 // Do not edit below this line
