aeb5cb13b1be00cc922d57aa10ee62e78463bfec
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 23:16:30 +0530

    Exercise five completed with all the test cases passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(parametes) {
+    var sum = 0;
+    var num1 = arguments[0] < arguments[1]? arguments[0] : arguments[1];
+    var num2 = arguments[0] > arguments[1]? arguments[0] : arguments[1];
+    
+    if(num1<0 || num2<0 || typeof(num1) != 'number' || typeof(num2) != 'number') {
+        return 'ERROR';
+    }
 
+    for(let i=num1; i<=num2; i++) {
+        sum += i;
+    }
+
+    return sum; 
 };
 
 // Do not edit below this line
