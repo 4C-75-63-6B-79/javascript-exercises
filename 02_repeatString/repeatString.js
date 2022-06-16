53f0c685834ec848cbf0996554ffcb3cd111609d
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 19:22:23 +0530

    fifth test case passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(word, times) {
     var output = '';
+    if(times < 0) {
+        return 'ERROR';
+    }
     for(let i =0; i<times; i++) {
         output = output + word;
     }
