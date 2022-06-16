8ab2a57d755d7c74d0d1e6049a770e17be8441b9
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 22:12:22 +0530

    The fourth exercise completed with all the test cases passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(parameters) {
+    var array = arguments[0];
+    /*for(let i=0; i<array.length; i++) {
+        if(array[i] === arguments[1]) {
+            array.splice(i,1);
+        }
+    }*/
 
+    for(i=1; i<arguments.length; i++) {
+        array = removeSingleNumber(array, arguments[i]);
+    }
+    return array;
 };
+
+function removeSingleNumber(array, number) {
+    for(let i=0; i<array.length; i++) {
+        if(array[i] === number) {
+            array.splice(i,1);
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
