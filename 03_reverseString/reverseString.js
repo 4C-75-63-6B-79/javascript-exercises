940173e0e47701ced5e17f7ac938d40af65f7eee
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 20:22:30 +0530

    Second exercise completed all four test cases passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    var output = '';
+    for(let i=0; i<word.length; i++) {
+        output = word.charAt(i) + output;
+    }
+    return output;
 };
 
 // Do not edit below this line
