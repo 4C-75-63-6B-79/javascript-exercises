cc033285630ea4ebe38a46ef5c4bf46a7122bae4
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-16 19:17:29 +0530

    First test case passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    var output = '';
+    for(let i =0; i<times; i++) {
+        output = output + word;
+    }
+    return output;
 };
 
 // Do not edit below this line
