6c5b117440090dd68ed40338065e8765a78178e9
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-27 21:19:23 +0530

    10 exercise is completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberAtIndex) {
+    numberAtIndex = Number(numberAtIndex);
+    if(numberAtIndex < 0) {
+        return 'OOPS';
+    }
+    let a = 1, b = 1;
+    let c = 0;
+    let index = 2;
+    if(numberAtIndex <3) {
+        return 1;
+    }
+    while(index < numberAtIndex) {
+        c = a + b;
+        a = b;
+        b = c;
+        index ++;
+    }
+    return (c);
 };
 
 // Do not edit below this line
