efd1554f0cead449da405b73d1531d17639b51de
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-27 21:09:33 +0530

    09 exercise is completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.split('');
+    word = word.filter((character) => character >= 'a' && character <= 'z');
+    word = word.join('');
+    return word.split('').reverse().join('') == word;
 };
 
 // Do not edit below this line
