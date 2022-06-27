982e5adf3b899c2a2b531e33245985559b993f51
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-27 21:42:45 +0530

    12 exercise is completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people = people.map((obj) => addAge(obj));
+    people.sort((obj1, obj2) => obj1.age - obj2.age);
+    // console.table(people);
+    return people[people.length - 1];
 };
+
+function addAge(obj) {
+    let yearOfBirth = obj.yearOfBirth;
+    let yearOfDeath = 'yearOfDeath' in obj ? obj.yearOfDeath : new Date().getFullYear();
+    obj['age'] = yearOfDeath - yearOfBirth;
+    return obj;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+28
+49
+29
