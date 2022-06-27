cef3e41e521bdeeb9d284d74e64b28973ac3ca35
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-27 20:56:25 +0530

    Exercise 08 was completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	let sum = numbers.reduce((previousValue, currentValue) => previousValue+currentValue , 0);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = numbers.reduce((previousValue, currentValue) => previousValue+currentValue , 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = numbers.reduce((previousValue, currentValue) => previousValue * currentValue, 1)
+  return product;
 };
 
-const power = function() {
-	
+const power = function(index, power) {
+	return Math.pow(index, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if(num1 == 0) {
+    return 1;
+  }
+  let arr = [...Array(num1 + 1).keys()].slice(1);
+  let factorial = arr.reduce((previousValue, currentValue) => previousValue * currentValue, 1);
+  return factorial;
 };
 
 // Do not edit below this line
